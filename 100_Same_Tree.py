ec3c2e1044f62d240f4b013a5ad68bba1dbd6d7c
Swayam595 <44289328+Swayam595@users.noreply.github.com>
2019-02-09 18:49:43 -0800

    Create 100_Same_Tree.py

---
+++ 100_Same_Tree.py
@@ -0,0 +1,6 @@
+def isSameTree(self, p: 'TreeNode', q: 'TreeNode') -> 'bool':
+        if p == None or q == None: return (p==q)  //  If the current node is NULL in either tree then check wheather both the node 
+                                                  //  of the trees are NULL is yes return true else false
+    
+        return (p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)) //  Check if current node values 
+                                              //  of the trees are same if true then go to the next node and check there node valuse 
